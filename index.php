424a2ce052b9deebcf0a61b8f91604bb1201eff4
Sadam <msadamramli@gmail.com>
2024-03-19 21:02:10 +0700

    final

--- index.php
+++ index.php
@@ -9,8 +9,14 @@
         <link rel="stylesheet"href="style.css">
     </head>
     <body>
+        <nav class="navbar">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Kalkulator BEP</span>
+                <span class="navbar-brand mb-0 h1">Pemweb SI-E</span>
+            </div>
+        </nav>
         <div class="isi">
-            <h1 class="container-md">Kalkulator Break Even Point</h1>
+            <h1 class="container-md header">Kalkulator Break Even Point dalam Rupiah</h1>
             <p class="container-md">Break Even Point atau disingkat dengan BEP merupakan suatu titik impas dimana laba yang didapatkan mempunyai nilai setara dengan yang diperlukan dalam sebuah usaha atau bisa disebut dengan tidak mengalami kerugian. </p>
             <div class="container-md">
                 <form action="proses.php" method="post" class="form">
@@ -37,6 +43,16 @@
                 ?>
             </div>
         </div>
+        <footer class="container-fluid">
+            <div class="nama-kelompok">
+                <p>Kelompok 3</p>
+            </div>
+            <ul>
+                <li>Muhammad Sadam Ramli</li>
+                <li>Azzahra Helga Voleta</li>
+                <li>Raffy Arydetya P</li>
+            </ul>
+        </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
--- proses.php
+++ proses.php
@@ -6,24 +6,40 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <link rel="stylesheet"href="style2.css">
+        <link rel="stylesheet"href="style.css">
     </head>
     <body>
-        <div class="isi">
+        <nav class="navbar">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Kalkulator BEP</span>
+                <span class="navbar-brand mb-0 h1">Pemweb SI-E</span>
+            </div>
+        </nav>
+        <div class="isi2">
             <h1>BEP dalam Rupiah:</h1>
             <div class="rumus">
                 <img src="img/rumus.png" alt="">
             </div>
             <div class="hasil">
-                <h2>Hasil BEP: <?php
+                <p>Hasil BEP: Rp <?php
                 $fixedCost = $_POST['fc'];
                 $variableCost = $_POST['vc'];
                 $salesVolume = $_POST['sv'];
 
                 $bep = $fixedCost/1-$variableCost/$salesVolume;
                 echo $bep;
-            ?></h2>
+            ?></p>
             </div>
         </div>
+        <footer class="container-fluid">
+            <div class="nama-kelompok">
+                <p>Kelompok 3</p>
+            </div>
+            <ul>
+                <li>Muhammad Sadam Ramli</li>
+                <li>Azzahra Helga Voleta</li>
+                <li>Raffy Arydetya P</li>
+            </ul>
+        </footer>
     </body>
 </html>
